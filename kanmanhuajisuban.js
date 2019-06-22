424337f1fd477e74a669ee5c2e2dfde135007dc1
imxh <37375495+imxh@users.noreply.github.com>
2019-06-22 20:57:36 +0800

    Update kanmanhuajisuban.js

--- kanmanhuajisuban.js
+++ kanmanhuajisuban.js
@@ -3,7 +3,7 @@ var url = $request.url;
 const path = "/app_api/v5/getuserinfo";
 if (url.indexOf(path) != -1) {
 	let obj = JSON.parse(body);
-	obj.data["isvip"] = "1";
+	obj["isvip"] = "1";
 	body = JSON.stringify(obj);
  }
 $done({body});
