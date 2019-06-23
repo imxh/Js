e0e47c6404ee44b592081f1b1fee056d0caf556d
imxh <37375495+imxh@users.noreply.github.com>
2019-06-23 12:12:03 +0800

    Update kanmanhuajisuban.js

--- kanmanhuajisuban.js
+++ kanmanhuajisuban.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var url = $request.url;
-const path = "/app_api/v5/getuserinfo";
+const path = "/app_api/v5/getuserinfo/";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
 	obj["isvip"] = "1";
