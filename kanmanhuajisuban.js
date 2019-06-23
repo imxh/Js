163eb248e2d9a95add54f24457ba870debdec931
imxh <37375495+imxh@users.noreply.github.com>
2019-06-23 19:10:46 +0800

    Update kanmanhuajisuban.js

--- kanmanhuajisuban.js
+++ kanmanhuajisuban.js
@@ -4,11 +4,13 @@ const path = "/app_api/v5/getuserinfo/";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
 	obj["isvip"] = "1";
+	obj["Uviptime"] = "1735729568";
 	body = JSON.stringify(obj);
  }
 $done({body});
 
 
+//看漫画极速版
 //By HoGer
 //hostname = getuserinfo.321mh.com
 //http-response ^https?://getuserinfo\.321mh\.com/app_api/v5/getuserinfo/
