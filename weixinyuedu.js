c07e6b7f7de163015feb2fa0fb7cc03e58678c37
imxh <37375495+imxh@users.noreply.github.com>
2019-07-02 03:06:06 +0800

    Update weixinyuedu.js

--- weixinyuedu.js
+++ weixinyuedu.js
@@ -4,9 +4,9 @@ const path = "/pay/memberCardSummary";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
         obj["expired"] = 0;
-        obj["permanent"] = 0;
 	obj["expiredTime"] = 1591804799;
 	obj["remainTime"] = 86313600;
+        obj["startTime"] = 1558360521;
 	body = JSON.stringify(obj);
  }
 $done({body});
