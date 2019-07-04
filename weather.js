83437aa6e7e1ffad9dfb61533f54e882caf142c3
imxh <37375495+imxh@users.noreply.github.com>
2019-07-04 09:40:11 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -8,9 +8,8 @@ $httpClient.get(weaapi, function(error, response, data){
         console.log(obj);
         var city = "所在城市：" + obj.city + "市";
         var wea = "天气状况：" + obj.wea + "  当前" + obj.tem + "℃  " + obj.tem2 + "℃～" + obj.tem1 + "℃";
-        var air = "当前风力：" + obj.win + obj.win_speed + "  风速" + obj.win_meter + "\n空气指数： " + obj.air + "  " + obj.air_level + "\n更新时间： " + obj.date + " "+ obj.week + " "+ obj.update_time;
-        let wmation = [city,wea,air];
-        $notification.post(wmation[0], wmation[1], wmation[2]);
+        var air = "当前风力：" + obj.win + obj.win_speed + "  风速" + obj.win_meter + "\n空气指数：" + obj.air + "  " + obj.air_level + "\n更新时间：" + obj.date + " "+ obj.week + " "+ obj.update_time;
+        $notification.post(city,wea,air);
         $done();
     }
 }
