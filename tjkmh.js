27b3d92fc69da0bf9aa9e774b9dc22a54cd34434
imxh <37375495+imxh@users.noreply.github.com>
2019-11-29 23:06:32 +0800

    Update tjkmh.js

--- tjkmh.js
+++ tjkmh.js
@@ -1,6 +1,6 @@
 var body = $response.body;
 var url = $request.url;
-const path = "/app_api/v5/getuserinfo/";
+const path = "/v1/getnewpayprice";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
 	obj.data.price = 0;
