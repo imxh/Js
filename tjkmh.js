6494053955d5200677ab5d93f770b7a22c4f83c1
imxh <37375495+imxh@users.noreply.github.com>
2019-11-29 22:35:17 +0800

    Update tjkmh.js

--- tjkmh.js
+++ tjkmh.js
@@ -3,8 +3,8 @@ var url = $request.url;
 const path = "/v1/getnewpayprice";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
-	obj["price"] = "0";
-  obj["cost_diamonds"] = "0";
+	obj.data.["price"] = "0";
+        obj.data.["cost_diamonds"] = "0";
 	body = JSON.stringify(obj);
  }
 $done({body});
