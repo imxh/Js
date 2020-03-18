395dd7b22dccd2052ef39bc78d97b1e761207355
imxh <37375495+imxh@users.noreply.github.com>
2020-03-18 23:37:21 +0800

    Delete tjkmh.js

--- tjkmh.js
+++
@@ -1,9 +0,0 @@
-var body = $response.body;
-var url = $request.url;
-const path = "/v1/getnewpayprice";
-let obj = JSON.parse(body);
-if (url.indexOf(path) != -1) {
-	obj.data.price = 0;
-	body = JSON.stringify(obj);
- }
-$done({body});
