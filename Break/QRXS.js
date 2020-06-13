6a4d492f5e4435604a69fc8e0947ece150e96f26
imxh <37375495+imxh@users.noreply.github.com>
2020-06-13 19:41:55 +0800

    Update QRXS.js

--- Break/QRXS.js
+++ Break/QRXS.js
@@ -1,5 +1,7 @@
 /*
-
+Surge4：
+http-response ^https?:\/\/api\.weiqire\.com/ios41\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Zymh.js
+MITM = api.weiqire.com
 */
 let url = $request.url;
 let body = JSON.parse($response.body);
